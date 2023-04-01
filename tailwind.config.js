ae695659d946efd20ea16ef96c34279db40312ec
vincent9705 <56332803+vincent9705@users.noreply.github.com>
2023-04-01 09:51:47 +0800

    (adding back tailwind css postcss)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: true, // or 'media' or 'class'
   theme: {
     extend: {},
