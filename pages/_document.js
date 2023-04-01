044058203acfaf072f1089bad209ea0e0b00875d
vincent9705 <56332803+vincent9705@users.noreply.github.com>
2023-04-01 22:06:07 +0800

    (pwa): added pwa function

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <link rel="apple-touch-icon" href="/icon-512x512.png" />
-        <link rel="manifest" href="/manifest.json" />
-      </Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icon.png"></link>
+          <meta name="theme-color" content="#fff" />
+        </Head>
       <body>
         <Main />
         <NextScript />
